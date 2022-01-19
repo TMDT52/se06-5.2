1cf511828f99f5eff7ba2824ab9d834d7d7d8522
tuanvukbc <66425034+tuanvukbc@users.noreply.github.com>
2022-01-19 17:29:49 +0700

    update

--- webfontend/application/views/site/header.php
+++ webfontend/application/views/site/header.php
@@ -1,15 +1,14 @@
 <div class="row" style="margin-top: 8px;height: 110px">
 			<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4 clearpadding">
-				<a href="<?php echo base_url(); ?>"><img src="<?php echo base_url(); ?>upload/logo.png" alt="" class="img-responsive"></a>
+				<a href="<?php echo base_url(); ?>"><img style="max-height: 110px;width: 80%" src="<?php echo base_url(); ?>upload/ilogo.png" alt="" class="img-responsive"></a>
 			</div>
 			<div class="col-xs-12 col-sm-8 col-md-8 col-lg-8 clearpadding">
-				<a href="#"><img style="max-height: 110px;width: 100%" src="<?php echo base_url(); ?>upload/banner.jpg" alt="" class="img-responsive"></a>
+				<a href="<?php echo base_url('khuyen-mai'); ?>"><img style="max-height: 110px;width: 100%" src="<?php echo base_url(); ?>upload/banner.jpg" alt="" class="img-responsive"></a>
 			</div>
 		</div>
 		<div class="row">
 			<nav class="navbar navbar-info re-navbar" >
 			  <div class="container-fluid re-container-fluid">
-			    <!-- Brand and toggle get grouped for better mobile display -->
 			    <div class="navbar-header">
 			      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
 			        <span class="sr-only">Toggle navigation</span>
@@ -19,7 +18,6 @@
 			      </button>
 			      	<a class="navbar-brand" href="#">--- Menu ---</a>
 			    </div>
-				<!-- Collect the nav links, forms, and other content for toggling -->
 			    <div class="collapse navbar-collapse re-navbar-collapse" id="bs-example-navbar-collapse-1">
 			      <ul class="nav navbar-nav">
 			        <li class="active"><a href="<?php echo base_url(); ?>"><span class="glyphicon glyphicon-home" aria-hidden="true"></span> HOME<span class="sr-only">(current)</span></a></li>
@@ -38,10 +36,10 @@
 			          <?php } ?>
 			          </ul>
 			        </li>
-			        <li><a href="<?php echo base_url('khuyen-mai'); ?>">Khuyến mại</a></li>
+			        <li><a href="<?php echo base_url('khuyen-mai'); ?>">Khuyến mãi Tết</a></li>
 			       </ul>
 			      <ul class="nav navbar-nav navbar-right">
-			        <?php $this->load->view('site/cart/cart_sh'); ?>
+			        <?php $this->load->view('site/cart/cart_sh'); ?> <!-- gio hang -->
 			        
 			        <?php if (!isset($user)) { ?>
 			        	<li><a href="<?php echo base_url('dang-nhap'); ?>">Đăng nhập</a></li>
@@ -56,8 +54,8 @@
 				        </li>
 			        <?php } ?>
 			      </ul>
-			    </div><!-- /.navbar-collapse -->
+			    </div>
 			    
-			  </div><!-- /.container-fluid -->
+			  </div>
 			</nav>
 		</div>
