8dea4cdda4a4acf23369629c3332c7ff3960ce7a
tuanvukbc <66425034+tuanvukbc@users.noreply.github.com>
2022-01-19 17:50:09 +0700

    Update head.php

--- webfontend/application/views/site/head.php
+++ webfontend/application/views/site/head.php
@@ -1,5 +1,5 @@
 	<meta charset="UTF-8">
-	<title>Shop quần áo Ngọc Lan</title>
+	<title>Shop thời trang SVT</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<script src="<?php echo public_url(); ?>js/jquery-3.1.1.js" type="text/javascript"></script>
