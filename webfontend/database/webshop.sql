1cb2aaf3891a69284aae03ba6233c51586b6e515
Khuất Đình Sơn <51318940+letgokds@users.noreply.github.com>
2022-01-24 00:27:39 +0700

    Update webshop.sql

--- webfontend/database/webshop.sql
+++ webfontend/database/webshop.sql
@@ -1,20 +1,9 @@
--- phpMyAdmin SQL Dump
--- version 4.4.12
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: May 18, 2017 at 05:43 AM
--- Server version: 5.6.25
--- PHP Version: 5.6.11
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `webshop`
